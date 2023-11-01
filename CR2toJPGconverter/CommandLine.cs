6e44273442b0bb0ca5a23eb4a2b199e43f4a21a2
lars- <lars-@HOME_PC>
2023-11-01 09:48:48 +0100

    Projektdateien hinzufügen.

---
+++ CR2toJPGconverter/CommandLine.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+public class Options
+{
+  [Value(0, MetaName = "directory", HelpText = "The path to the directory that contains the CR2 files.", Required = true)]
+  public string? Directory { get; set; }
+
+  [Option('r', "recursive", Default = false, HelpText = "Specifies whether subfolders should be searched recursively.")]
+  public bool Recursive { get; set; }
+
+  [Option('d', "delete", Default = false, HelpText = "Specifies whether the CR2 file should be deleted after conversion.")]
+  public bool Delete { get; set; }
+}
---
+++ CR2toJPGconverter/Program.cs
@@ -0,0 +1,69 @@
+﻿using CommandLine;
+using ImageMagick;
+using Serilog;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    ConfigureLogging();
+
+    Parser.Default.ParseArguments<Options>(args)
+        .WithParsed(opts => ConvertAndDeleteCR2(opts.Directory, opts.Recursive, opts.Delete))
+        .WithNotParsed<Options>((errs) => HandleParseError(errs));
+  }
+
+  static void ConfigureLogging()
+  {
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("conversion_log.txt")
+        .CreateLogger();
+  }
+
+  static void ConvertAndDeleteCR2(string? directory, bool recursive, bool delete)
+  {
+    if (string.IsNullOrEmpty(directory))
+    {
+      Log.Error("The directory cannot be null or empty.");
+      return;
+    }
+
+    if (recursive)
+    {
+      foreach (string subDirectory in Directory.GetDirectories(directory))
+      {
+        ConvertAndDeleteCR2(subDirectory, recursive, delete);
+      }
+    }
+
+    foreach (string file in Directory.GetFiles(directory, "*.cr2"))
+    {
+      string jpegFilePath = Path.ChangeExtension(file, ".jpg");
+      try
+      {
+        using (MagickImage image = new MagickImage(file))
+        {
+          image.Write(jpegFilePath);
+        }
+        if (delete)
+        {
+          File.Delete(file);
+          Log.Information($"Successfully converted and deleted {file}");
+        }
+        else
+        {
+          Log.Information($"Successfully converted {file}");
+        }
+      }
+      catch (Exception ex)
+      {
+        Log.Error($"Error converting {file}: {ex.Message}");
+      }
+    }
+  }
+  static void HandleParseError(IEnumerable<Error> errs)
+  {
+    // Optional: Handhabung von Parse-Fehlern, falls erforderlich
+  }
+}
